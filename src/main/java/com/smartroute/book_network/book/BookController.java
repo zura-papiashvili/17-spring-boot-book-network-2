df7c13a00ecbdee9bdd1c487088347f948a00915
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-23 15:59:59 -0500

    Update BookController.java

--- src/main/java/com/smartroute/book_network/book/BookController.java
+++ src/main/java/com/smartroute/book_network/book/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -15,7 +16,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.smartroute.book_network.common.PageResponse;
 
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
